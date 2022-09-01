3ecd795eb3e20974e1de3622e631c9d825dd33c9
Chrisluna10 <chrisluna4224@gmail.com>
2022-09-01 12:19:42 -0700

    user can now edit their email and username on account page

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -7,14 +7,35 @@ const FooterContainer = {
   bottom: 0,
   minHeight: 40,
   maxHeight: 100,
-  background: "#00a87e"
+  background: "#00a87e",
+}
+
+const textStyle = {
+  color: "white",
+  fontSize: "15px"
 }
 
 export default function Footer() {
   return (
-    <Grid style={FooterContainer}
+    <Grid
+      container
+      style={FooterContainer}
+      direction="row"
+      justifyContent="space-between"
     >
-      <Typography color="white">Footer</Typography>
+      <Grid item>
+        <Typography style={textStyle}>
+          Github for this code: https://github.com/Chrisluna10/marketplace-app
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography style={textStyle}>Created by: Chris Luna</Typography>
+      </Grid>
+      <Grid item>
+        <Typography style={textStyle}>
+          LinkedIn: https://www.linkedin.com/in/chris-luna-09a755219/
+        </Typography>
+      </Grid>
     </Grid>
   )
 }
