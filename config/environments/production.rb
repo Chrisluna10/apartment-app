76030b5bbacaff8004639784dd605a5e870a7caf
Chrisluna10 <chrisluna4224@gmail.com>
2022-09-04 11:32:46 -0700

    fixes host for fetch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "apartment_app_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url__options ={
+  config.action_mailer.default_url_options ={
     host: 'https://marketplace-app-cl.herokuapp.com/'
   }
 
