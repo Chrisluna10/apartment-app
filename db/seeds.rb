c214dbcc7af87bb8eb5ae5b4efb02a6d83871051
Chrisluna10 <chrisluna4224@gmail.com>
2022-09-07 15:22:16 -0700

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
 10.times do
   user = User.create!(
     username: Faker::Internet.username(specifier: 6..12),
@@ -18,36 +9,32 @@
     3.times do
     item = Item.create!(
       user: user,
-      name: Faker::Lorem.words.join(""),
-      category: "Electronics & Media",
-      price: Faker::Number.between(from: 5, to:500),
+      name: Faker::Vehicle.make_and_model,
+      category: "Vehicles",
+      price: Faker::Number.between(from: 5000, to:40000),
       description: Faker::Lorem.paragraphs.join("\n")
       )
-      @item.images.attach(io: File.open('app/javascript/assets/no_image.webp'), filename: 'no_image.webp')
+      item.image.attach(io: File.open('app/javascript/assets/no_image.webp'), filename: 'no_image.webp')
     end
     3.times do
       item = Item.create!(
         user: user,
-        name: Faker::Lorem.words.join(""),
-        category: "Home & Garden",
-        price: Faker::Number.between(from: 5, to:500),
+        name: Faker::Camera.brand_with_model,
+        category: "Electronics & Media",
+        price: Faker::Number.between(from: 300, to:2000),
         description: Faker::Lorem.paragraphs.join("\n")
         )
-        @item.images.attach(io: File.open('app/javascript/assets/no_image.webp'), filename: 'no_image.webp')
+        item.image.attach(io: File.open('app/javascript/assets/no_image.webp'), filename: 'no_image.webp')
       end
       3.times do
         item = Item.create!(
           user: user,
-          name: Faker::Lorem.words.join(""),
-          category: "Clothing, Shoes & Accessories",
-          price: Faker::Number.between(from: 5, to:500),
+          name: Faker::Game.title,
+          category: "Toys, Games & Hobbies",
+          price: Faker::Number.between(from: 5, to:60),
           description: Faker::Lorem.paragraphs.join("\n")
           )
-          @item.images.attach(io: File.open('app/javascript/assets/no_image.webp'), filename: 'no_image.webp')
+          item.image.attach(io: File.open('app/javascript/assets/no_image.webp'), filename: 'no_image.webp')
         end
 end
-
-# users.each do |attribute|
-#   User.create attribute
-# end
 
